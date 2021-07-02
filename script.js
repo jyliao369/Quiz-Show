23b0c345f306fc1abdcaa90fdc3e0d74bce4ab33
jyliao369 <jyliao369@gmail.com>
2021-07-02 19:31:44 -0400

    Created the Framework of the application as well as Added three test questions

---
+++ script.js
@@ -0,0 +1,43 @@
+
+// These are the questions that will be used in the quiz.
+// For now there are only three. More will be added. 
+var quizQues = [
+    {
+        question: "HTML is what type of language?",
+        
+        answers: {
+            a: 'Scripting Language',
+            b: 'Markup Language',
+            c: 'Programming Language',
+            d: 'Network Protocol'
+        },
+
+        correctAns: 'b'
+    },
+
+    {
+        question: "Which built-in method returns the calling string value converted to upper case?",
+        
+        answers: {
+            a: 'toUpperCase()',
+            b: 'toUpper()',
+            c: 'changeCase(case)',
+            d: 'None of the Above'
+        },
+
+        correctAns: 'a'
+    },
+
+    {
+        question: "What among the following is an appropriate when an event occurs when the user clicks on an element?",
+        
+        answers: {
+            a: 'onchange',
+            b: 'onkeyup',
+            c: 'onblur',
+            d: 'onclick'
+        },
+
+        correctAns: 'a'
+    },
+]
