43932b070823e019c6d7e3ba0379f4d786c40f00
jyliao369 <jyliao369@gmail.com>
2021-07-06 17:45:55 -0400

    Added a highscore area and users can add their name and score once the quiz is finished or done

--- script.js
+++ script.js
@@ -20,10 +20,14 @@ var choiceD = document.getElementById('choice3');
 // This is the start button. Once the button is pressed the timer should start
 // and the question/answers should be displayed.
 var startSwitch = document.getElementById('start')
-
 var switchEl = document.getElementById('switch');
 
 var introEl = document.getElementById('introText');
+
+var highName = document.getElementById('finalName');
+var highScore = document.getElementById('finalPoint');
+
+// var nameInput = document.querySelector("#name");
 
 // This variable will help determine which questions is being displayed. With it being
 // zero, this is the first question that is going to be displayed.
@@ -155,16 +159,18 @@ choiceB.style.display = "none";
 choiceC.style.display = "none";
 choiceD.style.display = "none";
 
+renderHighScore();
+
 function timer() {
 
     timeCount = 300;
     
     var countDown = setInterval(function() {
 
-        timerEl.textContent = timeCount + " Seconds Left";
         timeCount--;
+        timerEl.textContent = timeCount + " Seconds Left";
 
-        if (timeCount <= -1) {
+        if (timeCount <= -1 || currentQ === quizQues.length) {
             clearInterval(countDown);
             questionEl.style.display = "none";
 
@@ -177,6 +183,9 @@ function timer() {
             choiceB.style.display = "none";
             choiceC.style.display = "none";
             choiceD.style.display = "none";
+
+            introEl.textContent = "Game Over!!You Ran Out of Time! Try Again!!";
+            introEl.style.display = "block";
         }
 
     }, 1000);
@@ -220,7 +229,22 @@ function showQues() {
         choiceB.textContent = "";
         choiceC.textContent = "";
         choiceD.textContent = "";
+
+        buttonA.style.display = "none";
+        buttonB.style.display = "none";
+        buttonC.style.display = "none";
+        buttonD.style.display = "none";
+
     }
+}
+
+function renderHighScore() {
+
+    var names = localStorage.getItem("name");
+    var score2 = localStorage.getItem("point");
+
+    highName.textContent = names;
+    highScore.textContent = score2;
 }
 
 // This event listener should be able to start the timer and display
@@ -240,6 +264,9 @@ startSwitch.addEventListener("click", function() {
     choiceB.style.display = "block";
     choiceC.style.display = "block";
     choiceD.style.display = "block";
+
+    startSwitch.disabled = true;
+    
     timer();
     showQues();
 });
@@ -264,164 +291,18 @@ buttonD.addEventListener("click", function() {
     checkAns();
 });
 
+// This is the submit button where you users can submit there scores once the quiz
+// is over or they have finished taking the quiz. It should, also save the the name
+// and score in the highscore section of the page. 
 
+submitEl.addEventListener("click", function(event) {
+    event.preventDefault();
 
+    var name = document.querySelector("#name").value;
+    var score1 = score.toString();
 
+    localStorage.setItem("name", name);
+    localStorage.setItem("point", score1);
+    renderHighScore();
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// This function will compile the answers from the database of questions
-// It will use a for loop that will go down through the array and specifically pull each answer.
-// function getAnswer () {
-
-//     for (a = 0; a < quizQues.length; a++) {
-
-//         quizAns.push(quizQues[a].correctAns);
-
-//     }
-
-//     return quizAns;
-// }
-
-// function timer () {
-    
-//     // Currently, the time limit is only 10 seconds but this can be adjusted.
-//     timeCount = 11;
-//     var countDown = setInterval(function() {
-//         // console.log(timeCount);
-//         timeCount --;
-//         timerEl.textContent = timeCount + " seconds left";
-
-//         if(timeCount === 0) {
-//             clearInterval(countDown);
-//             timerEl.textContent = "Time's Up!! You Lose!!";
-//         }
-
-//     }, 1000);
-// }
-
-// // This function here is to display the question as well as display theAnswer choices for those specific questions. 
-// // Originally, I had it display the question in random order based on the length ofthe array. Now we should try to get 
-// // it to go in the order of the array. 
-// function showQues() {
-//     currentQues.textContent = quizQues[current].question;
-//     choiceA.textContent = quizQues[current].a;
-//     choiceB.textContent = quizQues[current].b;
-//     choiceC.textContent = quizQues[current].c;
-//     choiceD.textContent = quizQues[current].d;
-
-//     current++;
-// }
-
-// // This event listener should be able to start the timer and display
-// // the questions and the answers for the quiz. 
-// startSwitch.addEventListener("click", function() {
-//     timer();
-//     showQues();
-// });
-
-// // These event listeners' main function is to allow the user to select their answers. Once the
-// // answer is selected, there answer should be stored in an array that can then be compared to the
-// // real anaswers.
-// buttonA.addEventListener("click", function() {
-//     showQues();
-//     answer = ("a");
-//     console.log(answer);
-// });
-// buttonB.addEventListener("click", function() {
-//     showQues();
-//     answer = ("b");
-//     console.log(answer);
-// });
-// buttonC.addEventListener("click", function() {
-//     showQues();
-//     answer = ("c");
-//     console.log(answer);
-// });
-// buttonD.addEventListener("click", function() {
-//     showQues();
-//     answer = ("d");
-//     console.log(answer);
-// });
-
-// console.log(quizQues[0].correctAns);
-
-// // This function compares the answer of the the quiz answer and the answers that the user
-// // creates when they select their choices and determines which is right and wrong.
-// submitEl.addEventListener("click", function () {
-
-//     getAnswer();
-
-//     for (b = 0; b < answer.length; b++)  {
-
-//         if (answer[b] === quizAns[b]) {
-//             score++;
-//         }
-//     }
-//     console.log(score);
-// });
-
-
-// if (answerChoice !== quizQues[current].correctAns) {
-//     console.log("wrong answer");
-// } else {
-//     console.log("right answer");
-// }
